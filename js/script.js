79930286a0f53c38f2379597d17d8cafcd638baa
Bruna Uchida <brunatiemiuchida@gmail.com>
2024-05-15 11:37:09 -0300

    feat: add footer

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ document.addEventListener('keydown', (keyEvent) => {
     keyEvent.preventDefault()
     keyEvent.stopPropagation()
     let key = document.getElementById(keyEvent.key)
+    let code = keyEvent.key
 
     if (key == undefined) {
         key = document.getElementById(keyEvent.code)
+        code = keyEvent.code
     }
 
     if (key != undefined) {
-        document.getElementById('key-pressed').textContent = keyEvent.keyCode
+        document.getElementById('key-output').textContent = keyEvent.keyCode + ' - ' + code
         key.classList.add('key-pressed')
 
         setTimeout(() => {
